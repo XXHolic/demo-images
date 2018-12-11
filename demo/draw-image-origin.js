82f87f8a1e0b60baa5c94512204f798dc17ca7de
thy <972143891@qq.com>
2018-12-11 22:00:56 +0800

    feat: add origin canvas effect

---
+++ demo/draw-image-origin.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+
+  var canvas = document.getElementById('drawingCanvas');
+  var context = canvas.getContext('2d');
+  // canvas 的坐标原点是左上角，向右和向下是正值
+  // 图片 src: https://xxholic.github.io/lab/images/css-shape-other-five-start.png
+
+  var img = new Image();
+
+  img.onload = function() {
+    // 样式里面 canvas 用了 100% 虽然有效，但执行下面语句，会无效
+    context.drawImage(img, 0, 0, 300, 90);
+  };
+
+  img.src = './html5-rocks.png';
+
+}
