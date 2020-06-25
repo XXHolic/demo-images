741395a14822af057e5957b604c7aff5dbb77eed
thy <972143891@qq.com>
2020-06-25 18:09:16 +0800

    feat: add down script

--- py/ym.py
+++ py/ym.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 reqList="https://appapi2.gamersky.com/v5/getCMSNewsList"
 reqParams= {
@@ -24,7 +25,7 @@ reqParams= {
     "order":"timeDesc"
   }
 }
-list = []
+img_list = []
 
 def get_list(pageIndex=1,write=0):
   reqParams['request']['pageIndex'] = pageIndex
@@ -46,12 +47,55 @@ def write_json():
     filename = "../data/ym/"+ name
     with open(filename, "w+",encoding="utf-8") as f:
         f.write(back_data)
+        f.close()
     page_num += 1
   return
 
+# 获取图片链接
+def get_img_src():
+  page_num = 1
+  while page_num < 2:
+    name = "list"+ str(page_num) +".json"
+    filename = "../data/ym/"+ name
+    with open(filename, "r",encoding="utf-8") as f:
+        content = f.read()
+        # print(content)
+        data_dict = json.loads(content)
+        result = data_dict['result']
+        result_index = 0
+        result_len = len(result)
+        while result_index < result_index:
+          result_item = result[result_index]
+          img_list.append(result_item['thumbnails'][0])
+          result_index += 1
+        f.close()
+    page_num += 1
+  print(img_list)
+  return
 
-# backData = get_list()
-# list = backData['result']
+# 分析获取的 JSON 数据，获得图片
+def down_img():
+  img_index = 0
+  img_len = len(img_list)
+  while img_index < img_len:
+    img_item = img_list[img_index]
+    img_item_arr = img_item.split('/')
+    img_arr_len = len(img_item_arr)
+    filename = "../ym/cover/"+ img_item_arr[img_arr_len-1]
+    res = requests.get(img_item)
+    with open(filename, "wb") as f:
+        f.write(res.content)
+        print(img_index + filename + 'down success')
+        f.close()
+    img_index += 1
+  return
+
+
+# 获取列表数据
 # write_json()
-# print(backData)
-print('done')
+
+# 下载图片
+get_img_src()
+down_img()
+
+print('all done')
