7b741fd8b14984b721932e4ca2aeb1922124cbb5
thy <972143891@qq.com>
2020-12-12 23:39:42 +0800

    feat: add 50 chapter

--- py/get-comic.py
+++ py/get-comic.py
@@ -193,9 +193,9 @@ def clear_file(chapter):
   return
 
 def main():
-  startDire = 31
-  startDownChapter = 167777
-  while startDire <= 60:
+  startDire = 61
+  startDownChapter = 167807
+  while startDire <= 110:
     create_fold(str(startDire))
     get_img_address_html(str(startDire),str(startDownChapter))
     startDire += 1
