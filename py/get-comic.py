ef5d12dda668992ffb35e09d415a2280f4afd2af
thy <972143891@qq.com>
2020-12-14 20:33:32 +0800

    feat: add new method

--- py/get-comic.py
+++ py/get-comic.py
@@ -6,8 +6,23 @@ import os
 import urllib.parse
 import utils
 
+chapterListHeaders = {
+  "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+  "accept-encoding":"gzip, deflate, br",
+  "accept-language":"zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7",
+  "cache-control":"no-cache",
+  "cookie":"UM_distinctid=174a991ad1a26c-0afb998ad92617-3323767-1fa400-174a991ad1b5a8; __gads=ID=289a30228745382c-22cecc6d28c500f5:T=1607699470:RT=1607699470:R:S=ALNI_MbODWxVquBiHUrh8moe6CHup0kNMA; CNZZDATA1278325662=1030922568-1600569125-https%253A%252F%252Fcn.bing.com%252F%7C1607947301",
+  "Host":"www.manhuaniu.com",
+  "pragma":"no-cache",
+  "sec-fetch-dest":"document",
+  "sec-fetch-mode":"navigate",
+  "sec-fetch-site":"none",
+  "Sec-Fetch-User":"?1",
+  "Upgrade-Insecure-Requests":"1",
+  "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
+}
 
-html_req_headers = {
+chapterHeaders = {
   "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
   "accept-encoding":"gzip, deflate, br",
   "accept-language":"zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7",
@@ -15,7 +30,7 @@ html_req_headers = {
   "cookie":"UM_distinctid=174a991ad1a26c-0afb998ad92617-3323767-1fa400-174a991ad1b5a8; __gads=ID=289a30228745382c-22cecc6d28c500f5:T=1607699470:RT=1607699470:R:S=ALNI_MbODWxVquBiHUrh8moe6CHup0kNMA; CNZZDATA1278325662=1030922568-1600569125-https%253A%252F%252Fcn.bing.com%252F%7C1607772159",
   "Host":"www.manhuaniu.com",
   "pragma":"no-cache",
-  "referer":"https://www.manhuaniu.com/manhua/5298/",
+  "referer":"https://www.manhuaniu.com/manhua/15509/",
   "sec-fetch-dest":"document",
   "sec-fetch-mode":"navigate",
   "sec-fetch-site":"same-origin",
@@ -24,7 +39,7 @@ html_req_headers = {
   "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
 }
 
-img_req_header = {
+imgHeader = {
   "authority":"res.asemi.tech",
   "scheme":"https",
   "accept":"image/avif,image/webp,image/apng,image/*,*/*;q=0.8",
@@ -39,25 +54,18 @@ img_req_header = {
   "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
 }
 
-baseRoot= '../comic/guiDengDeLengChe/'
+baseRoot= '../comic/zhiBuMieDeNi/'
 maxPageNum = 200
 fileType = ".html"
-reqList="https://www.manhuaniu.com/manhua/5298/"
-
-# 一般单页
-# img_list = []
-
-# 连页的情况要单独处理
-contact_img = []
-contact_img_list = []
-
+reqList="https://www.manhuaniu.com/manhua/15509/"
+chapterFile = baseRoot + 'chapter.json'
 
 def create_fold(foldName):
   name = urllib.parse.unquote(foldName)
   utils.mkdir(baseRoot+name)
   return
 
-# 获得图片
+# 下载图片
 def down_img(localFold,data,isDirect):
   img_index = 0
   img_len = len(data)
@@ -73,11 +81,11 @@ def down_img(localFold,data,isDirect):
     else:
       img_item_spot_arr = img_item_arr_last.split('.')
       img_item_spot_len = len(img_item_spot_arr)
-      filename = baseRoot +fold_name+ str(imgCount) + '.' + img_item_spot_arr[img_item_spot_len-1]
+      filename = baseRoot +fold_name+ str(imgCount) + '.' + img_item_spot_arr[img_item_spot_len-1] + 'webp'
     if (os.path.exists(filename)):
       print(str(img_index)+' exists')
     else:
-      res = requests.get(img_item,headers=img_req_header)
+      res = requests.get(img_item,headers=imgHeader)
       # print(res.status_code)
       if (res.status_code == 200):
         with open(filename, "wb") as f:
@@ -118,7 +126,7 @@ def get_img_address_html(localFold,downChapter):
   while page_num <= 1:
     num_format = str(page_num)
     filename = reqList + downChapter+ fileType + "?p=" + num_format
-    res = requests.get(filename,headers=html_req_headers)
+    res = requests.get(filename,headers=chapterHeaders)
     titleText = 'not found'
     if (res.status_code == 200):
       # print(res.text)chapterImages
@@ -149,33 +157,32 @@ def get_img_address_html(localFold,downChapter):
   down_img(localFold,img_list,0)
   return
 
-# 处理连页的情况
-def deal_contact_img(chapter):
-  page_num = 1
-  fold_name = urllib.parse.unquote(chapter) + '/'
-  while page_num <= maxPageNum:
-    num_format = ""
-    if page_num<10:
-      num_format = '00'+str(page_num)
-    if (page_num >= 10 and page_num < 100):
-      num_format = '0'+str(page_num)
-    if (page_num >= 100):
-      num_format = str(page_num)
-    filePath = baseRoot +fold_name+ num_format+fileType
-    fsize = os.path.getsize(filePath)
-    if (fsize/1024 < 1):
-      contact_img.append(num_format)
-    page_num += 1
-  # print(contact_img)
-  img_index = 0
-  img_len = len(contact_img)
-  while img_index < img_len:
-    name = contact_img[img_index]+'-'+contact_img[img_index+1]
-    filename = reqList + name + fileType
-    contact_img_list.append(filename)
-    img_index += 2
-  # down_img(contact_img_list)
+
+# 获取章节数据
+def getChapterList():
+  reqUrl = 'https://www.manhuaniu.com/manhua/15509/'
+  res = requests.get(reqUrl,headers=chapterListHeaders)
+  # print(res.status_code)
+  if (res.status_code == 200):
+    pattern = re.compile(r'href="/manhua/15509/+.*?html" class="active"')
+    result = pattern.findall(res.text)
+    # print(result)
+    if (len(result)):
+      for index,value in enumerate(result):
+        valueSplit1 = value.split('.')
+        valueSplit2 = valueSplit1[0].split('/')
+        valueSplit2Len = len(valueSplit2)
+        result[index] = valueSplit2[valueSplit2Len-1]
+    # print(len(result))
+      with open(chapterFile, "wb") as f:
+        resultStr = json.dumps(result)
+        f.write(resultStr.encode(encoding='UTF-8'))
+        print('get chapter list success')
+        f.close()
+  else:
+    print('get chapter failed')
   return
+
 
 # 清理下载失败的文件
 def clear_file(chapter):
@@ -193,21 +200,22 @@ def clear_file(chapter):
   return
 
 def main():
-  startDire = 271
-  startDownChapter = 561198
-  while startDire <= 271:
-    create_fold(str(startDire))
-    get_img_address_html(str(startDire),str(startDownChapter))
-    startDire += 1
-    startDownChapter += 1
+  # getChapterList()
+  # return
+  with open(chapterFile, "r",encoding="utf-8") as f:
+      content = f.read()
+      chapterList = json.loads(content)
+      chapterNum = len(chapterList)
+      startDire = 1
+      while startDire <= 1:
+        startDownChapter = chapterList[startDire-1]
+        create_fold(str(startDire))
+        get_img_address_html(str(startDire),str(startDownChapter))
+        startDire += 1
   return
 
 # 主程序
 main()
-# create_fold()
-# deal_img()
-# deal_contact_img()
-# deal_wired_img()
 # clear_file() // 这个建议最后单独执行
 
 
