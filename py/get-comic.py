f28633c76f86a85a529291d266011a29b6dfdae2
thy <972143891@qq.com>
2021-03-03 23:29:42 +0800

    refactor: perf

--- py/get-comic.py
+++ py/get-comic.py
@@ -12,65 +12,17 @@ retryReq.mount('http://', HTTPAdapter(max_retries=2))
 retryReq.mount('https://', HTTPAdapter(max_retries=2))
 
 
-baseRoot= '../comic//'
+baseRoot= '../comic/test/'
 # baseRoot= '../comic//'
 maxPageNum = 200
 fileType = ".html"
-imgType = '.png'
-comicMark = '15403'
+comicMark = '933'
 reqList="https://www..com/manhua/" + comicMark + "/"
 chapterFile = baseRoot + 'chapter.json'
 imagesJsonFileName = 'images.json'
 emptyJsonFileName = 'empty.json'
 totalJsonFileName = 'total.json'
-preChapterFile = '../comic//chapter.json'
 
-chapterListHeaders = {
-  "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-  "accept-encoding":"gzip, deflate, br",
-  "accept-language":"zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7",
-  "cache-control":"no-cache",
-  "cookie":"UM_distinctid=174a991ad1a26c-0afb998ad92617-3323767-1fa400-174a991ad1b5a8; __gads=ID=289a30228745382c-22cecc6d28c500f5:T=1607699470:RT=1607699470:R:S=ALNI_MbODWxVquBiHUrh8moe6CHup0kNMA; CNZZDATA1278325662=1030922568-1600569125-https%253A%252F%252Fcn.bing.com%252F%7C1608114925",
-  "pragma":"no-cache",
-  "sec-fetch-dest":"document",
-  "sec-fetch-mode":"navigate",
-  "sec-fetch-site":"none",
-  "Sec-Fetch-User":"?1",
-  "Upgrade-Insecure-Requests":"1",
-  "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
-}
-
-chapterHeaders = {
-  "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-  "accept-encoding":"gzip, deflate, br",
-  "accept-language":"zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7",
-  "cache-control":"no-cache",
-  "cookie":"UM_distinctid=174a991ad1a26c-0afb998ad92617-3323767-1fa400-174a991ad1b5a8; __gads=ID=289a30228745382c-22cecc6d28c500f5:T=1607699470:RT=1607699470:R:S=ALNI_MbODWxVquBiHUrh8moe6CHup0kNMA; CNZZDATA1278325662=1030922568-1600569125-https%253A%252F%252Fcn.bing.com%252F%7C1608114925",
-  "Host":"www.manhuaniu.com",
-  "pragma":"no-cache",
-  "referer":reqList,
-  "sec-fetch-dest":"document",
-  "sec-fetch-mode":"navigate",
-  "sec-fetch-site":"same-origin",
-  "Sec-Fetch-User":"?1",
-  "Upgrade-Insecure-Requests":"1",
-  "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
-}
-
-imgHeader = {
-  "authority":"res.asemi.tech",
-  "scheme":"https",
-  "accept":"image/avif,image/webp,image/apng,image/*,*/*;q=0.8",
-  "accept-encoding":"gzip, deflate, br",
-  "accept-language":"zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7",
-  "cache-control":"no-cache",
-  "pragma":"no-cache",
-  "referer":"https://www.manhuaniu.com/",
-  "sec-fetch-dest":"image",
-  "sec-fetch-mode":"no-cors",
-  "sec-fetch-site":"cross-site",
-  "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
-}
 
 # 下载图片
 def down_img(localFold,data,isDirect):
@@ -98,13 +50,9 @@ def down_img(localFold,data,isDirect):
     imgCount = img_index+1
     if (isDirect):
       filename = baseRoot +fold_name + str(imgCount) + utils.getImageType(img_item)
-    else:
-      img_item_spot_arr = img_item_arr_last.split('.')
-      img_item_spot_len = len(img_item_spot_arr)
-      # filename = baseRoot +fold_name+ str(imgCount) + '.' + img_item_spot_arr[img_item_spot_len-1]
-      filename = baseRoot +fold_name+ str(imgCount) + '.' + img_item_spot_arr[img_item_spot_len-1] + imgType
+
     if (os.path.exists(filename)):
-      print(str(img_index)+' exists')
+      print('chapter '+ localFold + '-' +str(img_index)+' exists')
     else:
       res = retryReq.get(img_item,timeout=60)
       # print(res.status_code)
@@ -127,47 +75,6 @@ def down_img(localFold,data,isDirect):
       # print(resultStr)
       f.write(resultStr.encode(encoding='UTF-8'))
       f.close()
-  return
-
-# 获取图片下载的地址，这种适用图片地址无法看出规律，
-# 需要先请求 HTML 网页，返回网页里面会包含图片得地址
-# 同一章节的所有图片地址在一个页面上
-def get_img_address_html(localFold,downChapter):
-  page_num = 1
-  img_list=[]
-  # while page_num <= maxPageNum:
-  while page_num <= 1:
-    num_format = str(page_num)
-    filename = reqList + downChapter+ fileType + "?p=" + num_format
-    res = requests.get(filename,headers=chapterHeaders)
-    titleText = 'not found'
-    if (res.status_code == 200):
-      # print(res.text)chapterImages
-      pattern = re.compile(r'\["images/comic.*?\]')
-      titlePattern = re.compile(r'<h2>.*?<\/h2>')
-      result = pattern.findall(res.text)
-      titleResult = titlePattern.findall(res.text)
-      # print('result',titleResult)
-      if (len(result)):
-        img_list = json.loads(result[0])
-        for index,value in enumerate(img_list):
-          img_list[index] = 'https://res.asemi.tech//'+value
-        # print('img_list',img_list)
-      if (len(titleResult)):
-        titleText = titleResult[0]
-      #   img_list.append(fileAddress)
-      page_num += 1
-    else:
-      # 没有正确响应，认定没有接下来的图片了，就跳出循环
-      page_num = maxPageNum
-  # print(img_list)
-  name = "title.md"
-  fold_name = urllib.parse.unquote(localFold) + '/'
-  dataFilename = baseRoot+fold_name+ name
-  with open(dataFilename, "w+",encoding="utf-8") as f:
-      f.write(titleText)
-      f.close()
-  down_img(localFold,img_list,0)
   return
 
 # 获取图片下载的地址，这种适用图片地址无法看出规律，
@@ -276,7 +183,7 @@ def get_every_img_address_html(localFold,downChapter):
   return
 
 
-# 获取章节数据，并存放到本地
+# 获取所有章节数据，并存放到本地
 def getChaptersData():
   reqUrl = 'https://www..com/manhua/'+ comicMark +'/'
   res = requests.get(reqUrl)
@@ -285,25 +192,19 @@ def getChaptersData():
     pattern = re.compile(r'<ol class="links-of-books num_div"+.*?>([\s\S]*?)</ol*?>')
     versionResult = pattern.findall(res.text)
     # 可能有多个版本，例如黑白和彩色，所以这里需要再处理一次
-    pattern2 = re.compile(r'href="/manhua/119/+.*?html"')
+    pattern2 = re.compile(r'href="/manhua/'+comicMark+'/+.*?html"')
     result = pattern2.findall(versionResult[0])
-    # 前传
-    qianZhuan = []
+
     # 正传
     zhengZhuan = []
     # print(len(result))
     # print(result)
-    # return
     if (len(result)):
       for index,value in enumerate(result):
         valueSplit1 = value.split('.')
         valueSplit2 = valueSplit1[0].split('/')
         valueSplit2Len = len(valueSplit2)
         chapterLink = valueSplit2[valueSplit2Len-1]
-        # if(chapterLink >= 355906 and chapterLink <= 355971 ):
-        #   qianZhuan.append(chapterLink)
-        # else:
-        #   zhengZhuan.append(chapterLink)
         zhengZhuan.append(chapterLink)
 
 
@@ -313,11 +214,6 @@ def getChaptersData():
         f.write(resultStr.encode(encoding='UTF-8'))
         print('get chapter list success')
         f.close()
-      # with open(preChapterFile, "wb") as f:
-      #   resultStr = json.dumps(qianZhuan)
-      #   f.write(resultStr.encode(encoding='UTF-8'))
-      #   print('get qianZhuan chapter list success')
-      #   f.close()
   else:
     print('get chapter failed')
   return
@@ -354,7 +250,7 @@ def downAllImages():
   return
 
 def main():
-  # getChaptersData()
+  getChaptersData()
   # getImagesData()
   # downAllImages()
   return
--- py/utils.py
+++ py/utils.py
@@ -69,3 +69,51 @@ def createFold(baseRoot, foldName):
   name = urllib.parse.unquote(foldName)
   mkdir(baseRoot+name)
   return
+
+
+# 下面变量临时存放，可能用不到
+chapterHeaders = {
+  "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+  "accept-encoding":"gzip, deflate, br",
+  "accept-language":"zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7",
+  "cache-control":"no-cache",
+  "cookie":"UM_distinctid=174a991ad1a26c-0afb998ad92617-3323767-1fa400-174a991ad1b5a8; __gads=ID=289a30228745382c-22cecc6d28c500f5:T=1607699470:RT=1607699470:R:S=ALNI_MbODWxVquBiHUrh8moe6CHup0kNMA; CNZZDATA1278325662=1030922568-1600569125-https%253A%252F%252Fcn.bing.com%252F%7C1608114925",
+  "Host":"www.manhuaniu.com",
+  "pragma":"no-cache",
+  "sec-fetch-dest":"document",
+  "sec-fetch-mode":"navigate",
+  "sec-fetch-site":"same-origin",
+  "Sec-Fetch-User":"?1",
+  "Upgrade-Insecure-Requests":"1",
+  "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
+}
+
+chapterListHeaders = {
+  "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+  "accept-encoding":"gzip, deflate, br",
+  "accept-language":"zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7",
+  "cache-control":"no-cache",
+  "cookie":"UM_distinctid=174a991ad1a26c-0afb998ad92617-3323767-1fa400-174a991ad1b5a8; __gads=ID=289a30228745382c-22cecc6d28c500f5:T=1607699470:RT=1607699470:R:S=ALNI_MbODWxVquBiHUrh8moe6CHup0kNMA; CNZZDATA1278325662=1030922568-1600569125-https%253A%252F%252Fcn.bing.com%252F%7C1608114925",
+  "pragma":"no-cache",
+  "sec-fetch-dest":"document",
+  "sec-fetch-mode":"navigate",
+  "sec-fetch-site":"none",
+  "Sec-Fetch-User":"?1",
+  "Upgrade-Insecure-Requests":"1",
+  "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
+}
+
+imgHeader = {
+  "authority":"i.hamreus.com",
+  "scheme":"https",
+  "accept":"image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+  "accept-encoding":"gzip, deflate, br",
+  "accept-language":"zh-CN,zh;q=0.9",
+  "cache-control":"no-cache",
+  "pragma":"no-cache",
+  "referer":"https://www.mhgui.com/",
+  "sec-fetch-dest":"image",
+  "sec-fetch-mode":"no-cors",
+  "sec-fetch-site":"cross-site",
+  "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
+}
