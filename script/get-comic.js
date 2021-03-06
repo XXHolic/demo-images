f436b984021a272d22551770ddf9073aab51fc41
thy <972143891@qq.com>
2021-03-06 21:43:59 +0800

    refactor: add Adaptation

--- script/get-comic.js
+++ script/get-comic.js
@@ -1,12 +1,27 @@
 
 const fs = require('fs');
-const { createFold,LZString,imgHeader,writeLocalFile,requestPromise,readJsonFile } = require('./utils')
+const {
+  createFold,
+  writeLocalFile,
+  requestPromise,
+  readJsonFile,
+} = require('./utils')
+const {
+  getChapterContainerReg,
+  getChapterReg,
+  formatChapter,
+  sortChapterLink,
+  globalExpand,
+  getChapterImageData,
+  getImageType,
+  getImageHeader,
+} = require('./helper')
 
 const baseRoot = '../comic/huangJinShenWei/'
 let maxPageNum = 200
-const comicMark = '16306'
+const comicMark = 'huangjinshenwei'
 const imgHost = 'https://i.hamreus.com'
-const chapterReqUrl = "https://www.mhgui.com/comic/" + comicMark + '/'
+const chapterReqUrl = "https://www.ykmh.com/manhua/" + comicMark + '/'
 const chapterFile = baseRoot + 'chapter.json'
 const imagesJsonFileName = 'images.json'
 const titleFileName = 'title.md'
@@ -17,13 +32,13 @@ const baseEmptyJsonFile = baseRoot+'empty.json'
 async function getChaptersData() {
   const result = await requestPromise(`${chapterReqUrl}`)
 
-  const linkReg = /<div class="chapter-list cf mt10"+.*?>([\s\S]*?)<\/div*?>/g
+  const linkReg = getChapterContainerReg(2)
   const matchResult = result.match(linkReg)
   // console.log('---matchResult---')
   // console.log(matchResult)
 
   if (matchResult && matchResult.length) {
-    const chapterReg = new RegExp('href="\/comic\/'+comicMark+'\/+.*?html','g')
+    const chapterReg = getChapterReg(2,comicMark)
     // 默认先取第一个，一个结束后，手动修改
     const chapterLink = matchResult[0]
     const linkMatch = chapterLink.match(chapterReg)
@@ -32,22 +47,13 @@ async function getChaptersData() {
 
     if (linkMatch && linkMatch.length) {
       let allLink = linkMatch.reduce((acc,cur) => {
-        const valueSplit = cur.split('/')
-        const valueSplit2Len = valueSplit.length
-        const chapterLink = valueSplit[valueSplit2Len-1]
-        acc.unshift(chapterLink)
+        acc.push(formatChapter(cur,1))
         return acc
       },[])
     // console.log('---allLink---')
     // console.log(allLink)
-    // 有的情况可以根据数字大小排序,不需要的时候就注释掉
-      allLink = allLink.sort((a,b) => {
-        const aSplit = a.split('.')
-        const aNum = Number(aSplit[0])
-        const bSplit = b.split('.')
-        const bNum = Number(bSplit[0])
-        return aNum - bNum
-      })
+      // 有的情况可以根据数字大小排序
+      allLink = sortChapterLink(allLink,1)
       writeLocalFile(chapterFile,JSON.stringify(allLink))
       console.log('get all chapter links success')
     }
@@ -59,22 +65,19 @@ async function getChaptersData() {
 
 // 获取每个章节下所有图片的链接并存放到本地 json 文件
 async function getImagesData() {
-  // 针对特定网站的扩展，不同网站可能不一样
-  String.prototype.splic = function (f) {
-    return LZString.decompressFromBase64(this).split(f)
-  };
+  // 有就用
+  // globalExpand()
 
   const chapterList = readJsonFile(chapterFile)
   let baseFail = readJsonFile(baseEmptyJsonFile)
     // console.log('---chapterList---')
     // console.log(chapterList)
   const chapterNum = chapterList.length
-  let startDire = 204
-  while (startDire <= chapterNum) {
-  // while (startDire <= 1) { // 测试用
+  let startDire = 204 // 跟本地的文件夹命名顺序一致，从 1 开始
+  // while (startDire <= chapterNum) {
+  while (startDire <= 204) { // 测试用
     startDownChapter = chapterList[startDire-1]
     createFold(`${baseRoot}${startDire}`)
-    // getImageSrc(startDire,startDownChapter)
 
     const pathPre = `${baseRoot}${startDire}/`
     const imagesFile = `${pathPre}${imagesJsonFileName}`
@@ -94,96 +97,15 @@ async function getImagesData() {
       startDire += 1
       continue;
     }
-    const reg = /<script type="text\/javascript">window([\s\S]*?)<\/script*?>/g
-    const matchResult = res.match(reg)
-    // console.log('---matchResult---')
-    // console.log(matchResult)
 
-    if (matchResult && matchResult.length) {
-      const ele = matchResult[0];
-      const eleLen = ele.length;
-      const codeStr = ele.substring(57,eleLen-10)
-      // console.log('---codeStr---')
-      // console.log(codeStr)
-      const result = eval(codeStr)
-      // console.log('---result---')
-      // console.log(result)
-      const first = result.indexOf('{')
-      const last = result.lastIndexOf('}')
-
-      let newStr = result.substring(first,last+1)
-      const mainObj = JSON.parse(newStr)
-      const {cname,files,len,path,sl} = mainObj
-      const {e,m} = sl
-      const listData = files.reduce((acc,cur) => {
-        const url = `${imgHost}${path}${cur}?e=${e}&m=${m}`
-        acc.push(url)
-        return acc;
-      },[])
-      // console.log(listData)
-      const imagesContent = JSON.stringify({
-        total:len,
-        list:listData,
-      })
-
-      await writeLocalFile(imagesFile,imagesContent)
-      await writeLocalFile(titleFile,cname)
-      await console.log(`get chapter ${localFold} all image src`)
-    }
-
-    startDire += 1
-  }
-}
-
-// 针对所有的图片链接隐藏在同一个页面的情况
-async function getImageSrc(localFold,downChapter) {
-  const pathPre = `${baseRoot}${localFold}/`
-  const imagesFile = `${pathPre}${imagesJsonFileName}`
-  const titleFile = `${pathPre}${titleFileName}`
-  if (fs.existsSync(titleFile)) {
-    console.log(`chapter ${localFold} all image src exist`)
-    return;
-  }
-
-  const url = `${chapterReqUrl}${downChapter}`
-  const res = await requestPromise(url)
-  const reg = /<script type="text\/javascript">window([\s\S]*?)<\/script*?>/g
-  const matchResult = res.match(reg)
-  // console.log('---matchResult---')
-  // console.log(matchResult)
-
-  if (matchResult && matchResult.length) {
-    const ele = matchResult[0];
-    const eleLen = ele.length;
-    const codeStr = ele.substring(57,eleLen-10)
-    // console.log('---codeStr---')
-    // console.log(codeStr)
-    const result = eval(codeStr)
-    // console.log('---result---')
-    // console.log(result)
-    const first = result.indexOf('{')
-    const last = result.lastIndexOf('}')
-
-    let newStr = result.substring(first,last+1)
-    const mainObj = JSON.parse(newStr)
-    const {cname,files,len,path,sl} = mainObj
-    const {e,m} = sl
-    const listData = files.reduce((acc,cur) => {
-      const url = `${imgHost}${path}${cur}?e=${e}&m=${m}`
-      acc.push(url)
-      return acc;
-    },[])
-    // console.log(listData)
-    const imagesContent = JSON.stringify({
-      total:len,
-      list:listData,
-    })
+    const imageData = getChapterImageData(res,2)
+    const imagesContent = JSON.stringify(imageData)
 
     await writeLocalFile(imagesFile,imagesContent)
-    await writeLocalFile(titleFile,cname)
-    await console.log(`get chapter ${localFold} all image src`)
+    await writeLocalFile(titleFile,imageData.title)
+    await console.log(`get chapter ${startDire-1} all image src`)
+    startDire += 1
   }
-
 }
 
 async function downAllImages() {
@@ -191,28 +113,21 @@ async function downAllImages() {
     // console.log('---chapterList---')
     // console.log(chapterList)
   const chapterNum = chapterList.length
-  let startDire = 46
-  while (startDire <= chapterNum) {
-  // while (startDire <= 1) { // 测试用
+  let startDire = 204
+  // while (startDire <= chapterNum) {
+  while (startDire <= 204) { // 测试用
     const pre = `${baseRoot}${startDire}/`
     const emptyJsonFile = `${pre}${emptyJsonFileName}`
     const imgListFile = `${pre}${imagesJsonFileName}`
     const imagesContent = readJsonFile(imgListFile)
-    const {total,list} = JSON.parse(imagesContent)
+    const {total,list} = imagesContent
     let failData = readJsonFile(emptyJsonFile)
 
     const dataLen = list.length
     for (let index = 0; index < dataLen;index++) {
       const url = list[index];
       // console.info('url',url)
-      const split1 = url.split('?');
-      const split1Res = split1[0];
-      const split2 = split1Res.split('/');
-      const split2Res = split2[split2.length-1];
-      const reg = /\.[a-z]{1,4}/g;
-      const matchResult = split2Res.match(reg);
-      // const imagType = matchResult.join('')
-      const imagType = matchResult[0]
+      const imagType = getImageType(url,2)
       const imagePath = `${pre}${index+1}${imagType}`
       if (fs.existsSync(imagePath)) {
         console.log(`image exist ${imagePath}`)
@@ -221,6 +136,7 @@ async function downAllImages() {
       if (failData.includes(imagePath)) {
         continue;
       }
+      const imgHeader = getImageHeader(2)
       const res = await requestPromise(url,{encoding:'binary',headers:imgHeader}).catch((e) =>{
         console.log(`down fail ${imagePath}`)
         failData.push(url)
@@ -239,5 +155,5 @@ async function downAllImages() {
 }
 
 // getChaptersData()
-getImagesData()
-// downAllImages()
+// getImagesData()
+downAllImages()
---
+++ script/helper.js
@@ -0,0 +1,361 @@
+
+// 不同的网站，解析的正则不一样，在这里统一区分
+const getChapterContainerReg = (type) => {
+  switch(type){
+    case 1:
+    return /<div class="chapter-list cf mt10"+.*?>([\s\S]*?)<\/div*?>/g
+    case 2:
+    return /<div class="zj_list_con autoHeight"+.*?>([\s\S]*?)<\/div*?>/g
+  }
+}
+
+const getChapterReg = (type,comicMark) => {
+  switch(type){
+    case 1:
+    return new RegExp('href="\/comic\/'+comicMark+'\/+.*?html','g')
+    case 2:
+    return new RegExp('href="\/manhua\/'+comicMark+'\/+.*?html','g')
+  }
+}
+
+const formatChapter = (data,type) => {
+  let chapterLink = ''
+  if (type === 1) {
+    const valueSplit = data.split('/')
+    const valueSplit2Len = valueSplit.length
+    chapterLink = valueSplit[valueSplit2Len-1]
+  }
+
+  return chapterLink
+}
+
+const sortChapterLink = (data,type) => {
+  if (type === 1) {
+    // 有的情况可以根据数字大小排序
+    return data.sort((a,b) => {
+      const aSplit = a.split('.')
+      const aNum = Number(aSplit[0])
+      const bSplit = b.split('.')
+      const bNum = Number(bSplit[0])
+      return aNum - bNum
+    })
+  }
+}
+
+// 获取每个章节下所有图片链接
+const getChapterImageData = (pageData,type) => {
+  let data = {
+    list:[],
+    total:0,
+    title:''
+  }
+
+  if (type === 1) {
+    // 需要获取页面返回的函数，全局执行的情况，一般跟 globalExpand 配套
+    const reg = /<script type="text\/javascript">window([\s\S]*?)<\/script*?>/g
+    const matchResult = pageData.match(reg)
+    // console.log('---matchResult---')
+    // console.log(matchResult)
+
+    if (matchResult && matchResult.length) {
+      const ele = matchResult[0];
+      const eleLen = ele.length;
+      const codeStr = ele.substring(57,eleLen-10)
+      // console.log('---codeStr---')
+      // console.log(codeStr)
+      const result = eval(codeStr)
+      // console.log('---result---')
+      // console.log(result)
+      const first = result.indexOf('{')
+      const last = result.lastIndexOf('}')
+
+      let newStr = result.substring(first,last+1)
+      const mainObj = JSON.parse(newStr)
+      const {cname,files,len,path,sl} = mainObj
+      const {e,m} = sl
+      const listData = files.reduce((acc,cur) => {
+        const url = `${imgHost}${path}${cur}?e=${e}&m=${m}`
+        acc.push(url)
+        return acc;
+      },[])
+      // console.log(listData)
+      data.list = listData
+      data.total = len
+      data.title = cname
+    }
+  }
+
+  if (type === 2) {
+    const imagePrefix = 'https://pic.w1fl.com'
+    const reg = /<script>;var chapterImages([\s\S]*?)<\/script*?>/g
+    const matchResult = pageData.match(reg)
+    if (matchResult && matchResult.length) {
+      const content = matchResult[0];
+      const contentSplit = content.split(';')
+      // console.log('---contentSplit----')
+      // console.log(contentSplit)
+      const contentListStr = contentSplit[1]
+      // const eleLen = contentListStr.length;
+      const codeStr = contentListStr.substring(20)
+      // console.log('---codeStr---')
+      // console.log(codeStr)
+      let result = eval(codeStr)
+      result = result.map(ele => {
+        return `${imagePrefix}${ele}`
+      })
+
+      const titleContent = contentSplit[5]
+      const title = titleContent.substring(17,titleContent.length-1)
+      data.list = result
+      data.total = result.length
+      data.title = title
+    }
+  }
+
+  return data;
+}
+
+const getImageType = (data,type) => {
+  let fileType = '.jpg'
+  if (type === 1) {
+    const split1 = data.split('?');
+    const split1Res = split1[0];
+    const split2 = split1Res.split('/');
+    const split2Res = split2[split2.length-1];
+    const reg = /\.[a-z]{1,4}/g;
+    const matchResult = split2Res.match(reg);
+    // const imagType = matchResult.join('')
+    const imagType = matchResult[0]
+  }
+  if (type === 2) {
+    const split1 = data.split('/');
+    const split1Res = split1[split1.length-1];
+    const split2 = split1Res.split('.');
+    const split2Res = split2[split2.length-1];
+    fileType = `.${split2Res}`
+  }
+
+  return fileType
+}
+
+const getImageHeader = (type) => {
+  let headerParams = {
+    "scheme":"https",
+    "accept":"image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.9",
+    "accept-encoding":"gzip, deflate, br",
+    "accept-language":"zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+    "sec-fetch-dest":"image",
+    "sec-fetch-mode":"no-cors",
+    "sec-fetch-site":"cross-site",
+    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
+  }
+
+  if (type === 1) {
+    headerParams.authority = 'i.hamreus.com'
+    headerParams.referer = 'https://www.mhgui.com/'
+  }
+
+  if (type === 2) {
+    headerParams.authority = 'pic.w1fl.com'
+    headerParams.referer = 'https://www.ykmh.com/'
+  }
+
+  return headerParams
+}
+
+// 某一个网站特定的加密方法
+const LZString = (function () {
+  var f = String.fromCharCode;
+  var keyStrBase64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+  var baseReverseDic = {};
+
+  function getBaseValue(alphabet, character) {
+      if (!baseReverseDic[alphabet]) {
+          baseReverseDic[alphabet] = {};
+          for (var i = 0; i < alphabet.length; i++) {
+              baseReverseDic[alphabet][alphabet.charAt(i)] = i
+          }
+      }
+      return baseReverseDic[alphabet][character]
+  }
+  var LZString = {
+      decompressFromBase64: function (input) {
+          if (input == null) return "";
+          if (input == "") return null;
+          return LZString._0(input.length, 32, function (index) {
+              return getBaseValue(keyStrBase64, input.charAt(index))
+          })
+      },
+      _0: function (length, resetValue, getNextValue) {
+          var dictionary = [],
+              next, enlargeIn = 4,
+              dictSize = 4,
+              numBits = 3,
+              entry = "",
+              result = [],
+              i, w, bits, resb, maxpower, power, c, data = {
+                  val: getNextValue(0),
+                  position: resetValue,
+                  index: 1
+              };
+          for (i = 0; i < 3; i += 1) {
+              dictionary[i] = i
+          }
+          bits = 0;
+          maxpower = Math.pow(2, 2);
+          power = 1;
+          while (power != maxpower) {
+              resb = data.val & data.position;
+              data.position >>= 1;
+              if (data.position == 0) {
+                  data.position = resetValue;
+                  data.val = getNextValue(data.index++)
+              }
+              bits |= (resb > 0 ? 1 : 0) * power;
+              power <<= 1
+          }
+          switch (next = bits) {
+              case 0:
+                  bits = 0;
+                  maxpower = Math.pow(2, 8);
+                  power = 1;
+                  while (power != maxpower) {
+                      resb = data.val & data.position;
+                      data.position >>= 1;
+                      if (data.position == 0) {
+                          data.position = resetValue;
+                          data.val = getNextValue(data.index++)
+                      }
+                      bits |= (resb > 0 ? 1 : 0) * power;
+                      power <<= 1
+                  }
+                  c = f(bits);
+                  break;
+              case 1:
+                  bits = 0;
+                  maxpower = Math.pow(2, 16);
+                  power = 1;
+                  while (power != maxpower) {
+                      resb = data.val & data.position;
+                      data.position >>= 1;
+                      if (data.position == 0) {
+                          data.position = resetValue;
+                          data.val = getNextValue(data.index++)
+                      }
+                      bits |= (resb > 0 ? 1 : 0) * power;
+                      power <<= 1
+                  }
+                  c = f(bits);
+                  break;
+              case 2:
+                  return ""
+          }
+          dictionary[3] = c;
+          w = c;
+          result.push(c);
+          while (true) {
+              if (data.index > length) {
+                  return ""
+              }
+              bits = 0;
+              maxpower = Math.pow(2, numBits);
+              power = 1;
+              while (power != maxpower) {
+                  resb = data.val & data.position;
+                  data.position >>= 1;
+                  if (data.position == 0) {
+                      data.position = resetValue;
+                      data.val = getNextValue(data.index++)
+                  }
+                  bits |= (resb > 0 ? 1 : 0) * power;
+                  power <<= 1
+              }
+              switch (c = bits) {
+                  case 0:
+                      bits = 0;
+                      maxpower = Math.pow(2, 8);
+                      power = 1;
+                      while (power != maxpower) {
+                          resb = data.val & data.position;
+                          data.position >>= 1;
+                          if (data.position == 0) {
+                              data.position = resetValue;
+                              data.val = getNextValue(data.index++)
+                          }
+                          bits |= (resb > 0 ? 1 : 0) * power;
+                          power <<= 1
+                      }
+                      dictionary[dictSize++] = f(bits);
+                      c = dictSize - 1;
+                      enlargeIn--;
+                      break;
+                  case 1:
+                      bits = 0;
+                      maxpower = Math.pow(2, 16);
+                      power = 1;
+                      while (power != maxpower) {
+                          resb = data.val & data.position;
+                          data.position >>= 1;
+                          if (data.position == 0) {
+                              data.position = resetValue;
+                              data.val = getNextValue(data.index++)
+                          }
+                          bits |= (resb > 0 ? 1 : 0) * power;
+                          power <<= 1
+                      }
+                      dictionary[dictSize++] = f(bits);
+                      c = dictSize - 1;
+                      enlargeIn--;
+                      break;
+                  case 2:
+                      return result.join('')
+              }
+              if (enlargeIn == 0) {
+                  enlargeIn = Math.pow(2, numBits);
+                  numBits++
+              }
+              if (dictionary[c]) {
+                  entry = dictionary[c]
+              } else {
+                  if (c === dictSize) {
+                      entry = w + w.charAt(0)
+                  } else {
+                      return null
+                  }
+              }
+              result.push(entry);
+              dictionary[dictSize++] = w + entry.charAt(0);
+              enlargeIn--;
+              w = entry;
+              if (enlargeIn == 0) {
+                  enlargeIn = Math.pow(2, numBits);
+                  numBits++
+              }
+          }
+      }
+  };
+  return LZString
+})();
+
+const globalExpand = (type) => {
+
+  if (type === 1) {
+  // 针对特定网站的扩展，不同网站可能不一样
+    String.prototype.splic = function (f) {
+      return LZString.decompressFromBase64(this).split(f)
+    };
+  }
+
+}
+
+
+module.exports = {
+  getChapterContainerReg,
+  getChapterReg,
+  formatChapter,
+  sortChapterLink,
+  globalExpand,
+  getChapterImageData,
+  getImageType,
+  getImageHeader,
+}
